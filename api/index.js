6ef882d7f35005e7543abc2238f7608a2d785840
tushcmd <muturidavid854@gmail.com>
2024-05-24 00:52:19 +0300

    netlify

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
-import app from '../functions/app'
+import app from '../src/app';
 
 export default app;
--- index.js
+++ index.js
@@ -1,36 +1,6 @@
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
-import app from './functions/app.js';
-
-/*
-import express, { request, response } from 'express';
-import carRoutes from './routes/carRoutes.js';
-import cors from 'cors';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const app = express();
-
-//Middleware for parsing request body
-app.use(express.json());
-
-//Middleware for handling CORS Policy
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-  })
-);
-
-app.get('/', (request, response) => {
-  console.log(request);
-  return response.status(234).send('successful');
-});
-
-app.use('/cars', carRoutes);
-
-*/
+import app from './src/app.js'
 
 mongoose
   .connect(mongoDBURL)
@@ -43,5 +13,3 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
-
-//export default app;
